e6401ff78c3a63529d51c80101de437201672594
Ina Davydzenka <davydzenkaina@gmail.com>
2023-01-15 19:43:55 +0300

    changes pastebin

--- test/specs/practice-task1.js
+++ test/specs/practice-task1.js
@@ -3,7 +3,6 @@ const Pastebin = require ('../pageobjects/pastebin.page');
 describe('Should create New Paste on website Pastebin.com ',  () => {
     it ('Should create New Paste on website Pastebin.com with next params', async () => {
       Pastebin.open()
-    await browser.pause(5000)
     await Pastebin.createCode.addValue('Hello from WebDriver')
     await Pastebin.openPasteExpiration.click()
     await Pastebin.shoose10Minutes.click()
